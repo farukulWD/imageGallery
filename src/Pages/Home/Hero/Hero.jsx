90608f1dc6e658b44db82457fcda5f8572447de3
Farukul Islam <farukgb1999@gmail.com>
2023-10-18 08:34:26 +0600

    make hero section

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import HeroBg from "../../../assets/HeroImage/Herobg.jpg";
+import { BiSolidMicrophone } from "react-icons/bi";
 
 const Hero = () => {
   return (
@@ -6,20 +8,37 @@ const Hero = () => {
       <div
         className="hero min-h-screen"
         style={{
-          backgroundImage:
-            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+          backgroundImage: `url(${HeroBg})`,
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <div className="">
+            <h1 className="mb-5 text-2xl font-bold">
+              Download High Quality Images by creators
+            </h1>
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Over 2.4 million+ stock Images by our talented community
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <div class="  px-2">
+              <div class="max-w-md mx-auto rounded-lg overflow-hidden md:max-w-[800px]">
+                <div class="md:flex">
+                  <div class="w-full p-3">
+                    <div class="relative">
+                      <i class="absolute fa fa-search text-gray-400 top-5 left-4"></i>
+                      <input
+                        type="text"
+                        class="bg-white h-14 w-full px-12 rounded-lg focus:outline-none "
+                        name=""
+                      />
+                      <span class="absolute hover:cursor-pointer top-4 right-5 border-l pl-4">
+                        <BiSolidMicrophone></BiSolidMicrophone>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
